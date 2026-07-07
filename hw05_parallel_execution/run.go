ddb2059d26391bde697ecc7eb23aee9abca6a734
TheTeaToper <bulatov.maxim@gmail.com>
2026-07-08 00:49:30 +0300

    run.go

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -46,14 +46,13 @@ func Run(tasks []Task, n, m int) error {
 					}
 
 					err := taskForExecution()
-					if err != nil {
-						if m > 0 {
-							currentErrorsCount := atomic.AddInt32(&totalErrorsCount, 1)
-							if currentErrorsCount >= int32(m) {
-								cancelExecution()
-								return
-							}
-						}
+					if err == nil || m <= 0 {
+						return
+					}
+					currentErrorsCount := atomic.AddInt32(&totalErrorsCount, 1)
+					if currentErrorsCount >= int32(m) {
+						cancelExecution()
+						return
 					}
 				}
 			}
