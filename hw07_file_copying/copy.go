f85a18e8a09348e64ad40d3380b8b335bc8fa146
TheTeaToper <bulatov.maxim@gmail.com>
2026-07-23 21:17:09 +0300

    hw07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -9,7 +12,80 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+func Copy(fromPath, toPath string, offset, limit int64) error { //nolint:funlen
+	sourceFile, err := os.Open(fromPath) //nolint:gosec
+	if err != nil {
+		return fmt.Errorf("error of opening source file: %w", err)
+	}
+	defer func() {
+		_ = sourceFile.Close()
+	}()
+
+	sourceFileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return fmt.Errorf("error of getting source file info: %w", err)
+	}
+
+	if !sourceFileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	sourceFileSize := sourceFileInfo.Size()
+	if sourceFileSize < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	if _, err := sourceFile.Seek(offset, io.SeekStart); err != nil {
+		return fmt.Errorf("error of setting -offset in source file: %w", err)
+	}
+
+	availableBytes := sourceFileSize - offset
+
+	var totalBytesToCopy int64
+	if limit <= 0 || limit > availableBytes {
+		totalBytesToCopy = availableBytes
+	} else {
+		totalBytesToCopy = limit
+	}
+	if totalBytesToCopy == 0 {
+		fmt.Printf("Copying progress: %d%%", 100)
+		return nil
+	}
+
+	destinationFile, err := os.Create(toPath) //nolint:gosec
+	if err != nil {
+		return fmt.Errorf("error of creating destination file: %w", err)
+	}
+	defer func() {
+		_ = destinationFile.Close()
+	}()
+
+	limitedSourceReader := io.LimitReader(sourceFile, totalBytesToCopy)
+
+	buffer := make([]byte, BUFFER_SIZE)
+	var copiedBytes int64
+
+	fmt.Printf("Copying progress: %d%%", 0)
+	for {
+		readBytes, readingErr := limitedSourceReader.Read(buffer)
+		if readingErr != nil && readingErr != io.EOF {
+			return fmt.Errorf("error of reading from source file to buffer: %w", readingErr)
+		}
+		if readBytes > 0 {
+			_, writingErr := destinationFile.Write(buffer[:readBytes])
+			if writingErr != nil {
+				return fmt.Errorf("errof of writing buffer to destination file: %w", writingErr)
+			}
+			copiedBytes += int64(readBytes)
+			progress := (copiedBytes * 100) / totalBytesToCopy
+			fmt.Printf("\rCopying progress: %d%%", progress)
+		}
+
+		if readingErr == io.EOF {
+			break
+		}
+	}
+	if err := destinationFile.Sync(); err != nil {
+		return err
+	}
 	return nil
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
 	from, to      string
 	limit, offset int64
 )
+
+const BUFFER_SIZE = 1024
 
 func init() {
 	flag.StringVar(&from, "from", "", "file to read from")
@@ -19,4 +23,17 @@ func init() {
 func main() {
 	flag.Parse()
 	// Place your code here.
+	if from == "" {
+		_ = fmt.Errorf("-from arg is empty")
+		os.Exit(1)
+	}
+	if to == "" {
+		_ = fmt.Errorf("-to arg is empty")
+		os.Exit(1)
+	}
+	if err := Copy(from, to, offset, limit); err != nil {
+		_ = fmt.Errorf("file copying error: %w", err)
+		os.Exit(1)
+	}
+	fmt.Printf("File %s successfully copied to %s", from, to)
 }
