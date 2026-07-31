b1377415e825bb87335cff16f29b6d67d577b029
TheTeaToper <bulatov.maxim@gmail.com>
2026-07-31 21:57:18 +0300

    hw08

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	// Place your code here.
+	if len(os.Args) < 3 {
+		fmt.Println("3 args minimun")
+		os.Exit(1)
+	}
+	directory := os.Args[1]
+	environments, err := ReadDir(directory)
+	if err != nil {
+		fmt.Println("error of reading directory: %w", err)
+		os.Exit(1)
+	}
+	cmd := os.Args[2]
+	args := os.Args[3:]
+
+	result := RunCmd(append([]string{cmd}, args...), environments)
+	os.Exit(result)
 }
